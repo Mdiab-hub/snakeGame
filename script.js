e06b5296f721df936b9dce125ec3ce521b26cac2
Mdiab-hub <motasem.diab@gmail.com>
2021-06-27 09:06:20 -0400

    added some features

---
+++ script.js
@@ -0,0 +1,44 @@
+var canvas;
+var canvasContext;
+var ballx = 50;
+var ballspeedx = 15;
+
+window.onload = function(){
+    canvas = document.getElementById('gameCanvas');
+    canvasContext = canvas.getContext('2d');
+    
+    var framesPerSecond = 30;
+    setInterval(function(){
+        moveEverything();
+        drawEverything();
+    }, 1000/framesPerSecond);
+    canvasContext.fillStyle = 'green';
+    canvasContext.fillRect(ballx,230,10,10);
+}
+
+function moveEverything(){
+    ballx = ballx + ballspeedx;
+    if(ballx < 0){
+        ballspeedx = -ballspeedx;
+        // alert("Hit wall")
+    }
+    if(ballx > canvas.width){
+        ballspeedx = -ballspeedx;
+        // alert("Hit wall")
+    }
+
+}
+
+function drawEverything() {
+
+
+    canvasContext.fillStyle = 'white';
+    canvasContext.fillRect(0,0,canvas.width,canvas.height);
+    canvasContext.fillStyle = 'green';
+    canvasContext.fillRect(ballx,230,10,10);
+    
+}
+
+// function createSecondBox(){
+    
+// }
